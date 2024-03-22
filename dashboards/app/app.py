b7b1b39d269318fc4cc9ec9dd2feebf85a50abd5
KyllianBeguin <kyllianbeguin@vivaldi.net>
2024-03-22 15:22:33 -0400

    [kb] ♻️ Improve code quality

--- dashboards/app/app.py
+++ dashboards/app/app.py
@@ -1,18 +1,25 @@
 import dash
-from dash import Dash, html, dcc#, callback, Output, Input 
-import plotly.express as px
+from dash import Dash, dcc, html  # , callback, Output, Input
 
 app = Dash(__name__, use_pages=True)
 
-app.layout = html.Div([
-    html.H1(children='ZeroDechetSauvage', style={'textAlign':'center'}),
-    html.Div([
+app.layout = html.Div(
+    [
+        html.H1(children="ZeroDechetSauvage", style={"textAlign": "center"}),
         html.Div(
-            dcc.Link(f"Onglet {str(page['name']).upper()}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
-    ]),
-    dash.page_container,
-])
+            [
+                html.Div(
+                    dcc.Link(
+                        f"Onglet {str(page['name']).upper()}",
+                        href=page["relative_path"],
+                    ),
+                )
+                for page in dash.page_registry.values()
+            ],
+        ),
+        dash.page_container,
+    ],
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8050", debug=True)
