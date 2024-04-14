5f66eb16d630e09d0315bea153f94d42a1793d22
DridrM <hmoncomble@gmail.com>
2024-04-14 23:42:09 +0200

    Add params.py into hotspots_function and first attempt to plot a map on the hotspot tab

--- dashboards/app/pages/hotspots.py
+++ dashboards/app/pages/hotspots.py
@@ -4,46 +4,49 @@ import pandas as pd
 import duckdb
 
 from hotspots_functions.maps import plot_adopted_waste_spots
-
-st.markdown(
-    """# 🔥 Hotspots
-*Quelles sont les zones les plus impactées ?*
-"""
+from hotspots_functions.params import (
+    NB_DECHETS_PATH,
+    DATA_ZDS_PATH,
+    REGION_GEOJSON_PATH,
 )
 
-df_nb_dechet = pd.read_csv(
-    (
-        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
-        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
-        "sation/data/data_releve_nb_dechet.csv"
-    )
-)
 
-df_other = pd.read_csv(
-    (
-        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
-        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
-        "sation/data/data_zds_enriched.csv"
-    )
-)
+##################
+# 1/ Import data #
+##################
 
-res_aggCategory_filGroup = duckdb.query(
-    (
-        "SELECT categorie, sum(nb_dechet) AS total_dechet "
-        "FROM df_nb_dechet "
-        "WHERE type_regroupement = 'GROUPE' "
-        "GROUP BY categorie "
-        "HAVING sum(nb_dechet) > 10000 "
-        "ORDER BY total_dechet DESC;"
-    )
-).to_df()
+# nb dechets : Unused for now
+df_nb_dechets = pd.read_csv(NB_DECHETS_PATH)
 
-st.altair_chart(
-    alt.Chart(res_aggCategory_filGroup)
-    .mark_bar()
-    .encode(
-        x=alt.X("categorie", sort = None, title = ""),
-        y=alt.Y("total_dechet", title = "Total de déchet"),
-    ),
-    use_container_width = True,
-)
+# data_zds : main source of data for the hotspots tab
+data_zds = pd.read_csv(DATA_ZDS_PATH)
+
+##################
+# 2/ Hotspot tab #
+##################
+
+# Tab title
+st.markdown("""# 🔥 Hotspots : **Quelles sont les zones les plus impactées ?**""")
+
+################################
+# 2.1/ Carte des spots adoptés #
+################################
+
+# Create 2 columns for 2 filters
+columns = st.columns(2)
+
+# Choice of the region
+x1 = data_zds["REGION"].unique()
+f1 = columns[0].selectbox("Seléctionnez une région (par défaut votre région) :", x1)
+columns[0].write(f1)
+
+# Choice of the environment
+x2 = data_zds["TYPE_MILIEU"].unique()
+f2 = columns[0].selectbox("Seléctionnez un environnement :", x2)
+columns[0].write(f2)
+
+# Create the filter dict
+filter_dict = {"REGION": f1, "TYPE_MILIEU": f2}
+
+# Create the map of the adopted spots
+plot_adopted_waste_spots(data_zds, filter_dict, region_geojson_path=REGION_GEOJSON_PATH)
