a36f44dccc454b4468537b63d018b5ace7c620ae
Floriane Duccini <floriane@duccini.com>
2024-04-19 14:05:27 +0200

    first draft onglet structure

--- dashboards/app/home.py
+++ dashboards/app/home.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 st.markdown(
@@ -8,4 +9,111 @@ st.markdown(
 )
 
 st.markdown("""# À propos""")
-st.image("media/ZDS-logo.png")
+
+
+# Chargement des données et filtre géographique à l'arrivée sur le dashboard
+# Table des volumes par matériaux
+@st.cache_data
+def load_df_other() -> pd.DataFrame:
+    df = pd.read_csv(
+        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
+        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
+        "sation/data/data_zds_enriched.csv",
+    )
+    # Ajout des colonnes DEP_CODE_NOM et COMMUNE_CODE_NOM qui concatenent le numéro INSEE
+    # et le nom de l'entité géographique (ex : 13 - Bouches du Rhône)
+    df["DEP_CODE_NOM"] = df["DEP"] + " - " + df["DEPARTEMENT"]
+    df["COMMUNE_CODE_NOM"] = df["INSEE_COM"] + " - " + df["commune"]
+    return df
+
+
+# Table des structures
+@st.cache_data
+def load_structures() -> pd.DataFrame:
+    return pd.read_excel(
+        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
+        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
+        "sation/data/export_structures_29022024%20(1).xlsx",
+        index_col=0,
+    )
+
+
+# Appel des fonctions pour charger les données
+
+df_other = load_df_other()
+df_structures = load_structures()
+
+
+# Création du filtre par niveau géographique : correspondance labels et variables du dataframe
+niveaux_admin_dict = {
+    "Région": "REGION",
+    "Département": "DEP_CODE_NOM",
+    "EPCI": "LIBEPCI",
+    "Commune": "COMMUNE_CODE_NOM",
+}
+
+# 1ère étape : sélection du niveau administratif concerné (région, dép...)
+# Si déjà saisi précédemment, initialiser le filtre avec les valeurs entrées précédemment
+# Récupérer les index pour conserver la valeur des filtres au changement de pages
+# Filtre niveau administratif
+niveau_admin = st.session_state.get("niveau_admin", None)
+index_admin = st.session_state.get("index_admin", None)
+# Filtre collectivité
+collectivite = st.session_state.get("collectivite", None)
+index_collec = st.session_state.get("index_collec", None)
+
+# Initialiser la selectbox avec l'index récupéré
+select_niveauadmin = st.selectbox(
+    "Niveau administratif : ",
+    niveaux_admin_dict.keys(),
+    index=index_admin,
+)
+
+if select_niveauadmin is not None:
+    # Filtrer la liste des collectivités en fonction du niveau admin
+    liste_collectivites = df_other[niveaux_admin_dict[select_niveauadmin]]
+    liste_collectivites = liste_collectivites.sort_values().unique()
+
+    # 2ème filtre : sélection de la collectivité concernée
+    select_collectivite = st.selectbox(
+        "Collectivité : ",
+        liste_collectivites,
+        index=index_collec,
+    )
+
+
+if st.button("Enregistrer la sélection"):
+    # Enregistrer les valeurs sélectionnées dans le session.state
+    st.session_state["niveau_admin"] = select_niveauadmin
+    st.session_state["index_admin"] = list(niveaux_admin_dict.keys()).index(
+        select_niveauadmin,
+    )
+
+    st.session_state["collectivite"] = select_collectivite
+    st.session_state["index_collec"] = list(liste_collectivites).index(
+        select_collectivite,
+    )
+
+    # Afficher la collectivité sélectionnée
+    st.write(f"Vous avez sélectionné : {select_niveauadmin} {select_collectivite}.")
+
+    # Filtrer et enregistrer le DataFrame dans un session state pour la suite
+    colonne_filtre = niveaux_admin_dict[select_niveauadmin]
+    df_other_filtre = df_other[df_other[colonne_filtre] == select_collectivite]
+    st.session_state["df_other_filtre"] = df_other_filtre
+
+    # Filtrer dataframe structures et enregistrer dans le session.state
+    st.session_state["structures"] = df_structures
+
+    # Filtrer et enregistrer le dataframe nb_dechets dans session.State
+    # Récuperer la liste des relevés
+    id_releves = df_other_filtre["ID_RELEVE"].unique()
+    # Filtrer df_nb_dechets sur la liste des relevés
+    # st.session_state["df_nb_dechets_filtre"] = df_nb_dechets[
+
+    # Afficher le nombre de relevés disponibles
+    nb_releves = len(st.session_state["df_other_filtre"])
+    st.write(
+        f"{nb_releves} relevés de collecte sont disponibles \
+             pour l'analyse sur votre territoire.",
+    )
--- dashboards/app/pages/structures.py
+++ dashboards/app/pages/structures.py
@@ -2,6 +2,13 @@ import streamlit as st
 import altair as alt
 import duckdb
 import pandas as pd
+import plotly.express as px
+
+
+# Configuration de la page
+st.set_page_config(
+    layout="wide", page_title="Dashboard Zéro Déchet Sauvage : onglet Structures"
+)
 
 st.markdown(
     """# 🔭 Structures
@@ -9,41 +16,142 @@ st.markdown(
 """
 )
 
-df_nb_dechet = pd.read_csv(
-    (
-        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
-        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
-        "sation/data/data_releve_nb_dechet.csv"
+
+# Récupérer les filtres géographiques s'ils ont été fixés
+filtre_niveau = st.session_state.get("niveau_admin", "")
+filtre_collectivite = st.session_state.get("collectivite", "")
+
+
+# Appeler les dataframes filtrés depuis le session state
+if "structures" not in st.session_state:
+    st.write(
+        """
+            ### :warning: Merci de sélectionner une collectivité\
+            dans l'onglet Home pour afficher les données. :warning:
+            """
     )
-)
+    st.stop()
+else:
+    df_structures = st.session_state["structures"]
 
-df_other = pd.read_csv(
-    (
-        "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
-        "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
-        "sation/data/data_zds_enriched.csv"
+# # df_nb_dechet = pd.read_csv(
+# #     (
+# #         "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
+# #         "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
+# #         "sation/data/data_releve_nb_dechet.csv"
+# #     )
+# # )
+
+# # df_other = pd.read_csv(
+# #     (
+# #         "https://github.com/dataforgoodfr/12_zero_dechet_sauvage/raw/2-"
+# #         "nettoyage-et-augmentation-des-donn%C3%A9es/Exploration_visuali"
+# #         "sation/data/data_zds_enriched.csv"
+# #     )
+# # )
+
+# # res_aggCategory_filGroup = duckdb.query(
+# #     (
+# #         "SELECT categorie, sum(nb_dechet) AS total_dechet "
+# #         "FROM df_nb_dechet "
+# #         "WHERE type_regroupement = 'GROUPE' "
+# #         "GROUP BY categorie "
+# #         "HAVING sum(nb_dechet) > 10000 "
+# #         "ORDER BY total_dechet DESC;"
+# #     )
+# # ).to_df()
+
+# # st.bar_chart(data=res_aggCategory_filGroup, x="categorie", y="total_dechet")
+
+# st.altair_chart(
+#     alt.Chart(res_aggCategory_filGroup)
+#     .mark_bar()
+#     .encode(
+#         x=alt.X("categorie", sort=None, title=""),
+#         y=alt.Y("total_dechet", title="Total de déchet"),
+#     ),
+#     use_container_width=True,
+# )
+
+if filtre_niveau == "" and filtre_collectivite == "":
+    st.write("Aucune sélection de territoire n'a été effectuée")
+else:
+    st.write(f"Votre territoire : {filtre_niveau} {filtre_collectivite}")
+
+
+# Ligne 1 : 2 cellules avec les indicateurs clés en haut de page
+l1_col1, l1_col2 = st.columns(2)
+
+# Pour avoir une  bordure, il faut nester un st.container dans chaque colonne (pas d'option bordure dans st.column)
+
+# 1ère métrique : nombre d'acteurs
+cell1 = l1_col1.container(border=True)
+nb_acteurs = len(df_structures)
+# Trick pour séparer les milliers
+cell1.metric("Acteurs présents sur le territoire", nb_acteurs)
+
+# 2ème métrique : nb de spots adoptés
+cell2 = l1_col2.container(border=True)
+nb_spots_adoptes = df_structures["A1S_NB_SPOTS_ADOPTES"].sum()
+cell2.metric("Spots adoptés", nb_spots_adoptes)
+
+
+# Ligne 2 : 2 graphiques en ligne : carte et pie chart type de structures
+
+# l2_col1, l2_col2 = st.columns(2)
+# cell4 = l2_col1.container(border=True)
+# cell5 = l2_col2.container(border=True)
+
+with st.container():
+
+    df_aggType = duckdb.query(
+        (
+            "SELECT TYPE, count(TYPE) AS nb_structures "
+            "FROM df_structures "
+            "GROUP BY TYPE "
+            "ORDER BY nb_structures DESC;"
+        )
+    ).to_df()
+
+    # Création du diagramme en donut en utilisant le dictionnaire de couleurs pour la correspondance
+    fig = px.pie(
+        df_aggType,
+        values="nb_structures",
+        names="TYPE",
+        title="Répartition des types de structures",
+        hole=0.4,
+        color="TYPE",
     )
-)
 
-res_aggCategory_filGroup = duckdb.query(
-    (
-        "SELECT categorie, sum(nb_dechet) AS total_dechet "
-        "FROM df_nb_dechet "
-        "WHERE type_regroupement = 'GROUPE' "
-        "GROUP BY categorie "
-        "HAVING sum(nb_dechet) > 10000 "
-        "ORDER BY total_dechet DESC;"
+    # Amélioration de l'affichage
+    # Change the percent format to round to integer
+    fig.update_traces(
+        textinfo="percent",
+        texttemplate="%{percent:.0%}",
+        textfont_size=16,
     )
-).to_df()
+    fig.update_layout(
+        autosize=True,
+        legend_title_text="Type de structure",
+    )
 
-# st.bar_chart(data=res_aggCategory_filGroup, x="categorie", y="total_dechet")
+    # Affichage du graphique
+    st.plotly_chart(fig, use_container_width=True)
 
-st.altair_chart(
-    alt.Chart(res_aggCategory_filGroup)
-    .mark_bar()
-    .encode(
-        x=alt.X("categorie", sort=None, title=""),
-        y=alt.Y("total_dechet", title="Total de déchet"),
-    ),
-    use_container_width=True,
-)
+# Cartographie des structures
+with st.container():
+    st.markdown(""" **Cartographie des structures du territoire**""")
+
+
+# Affichage du dataframe
+with st.container():
+    st.markdown(""" **Structures du territoire**""")
+    df_struct_simplifie = duckdb.query(
+        (
+            "SELECT NOM as Nom, TYPE, ACTION_RAMASSAGE AS 'Nombre de collectes', A1S_NB_SPOTS_ADOPTES as 'Nombre de spots adoptés' "
+            "FROM df_structures "
+            "ORDER BY Nom DESC;"
+        )
+    ).to_df()
+
+    st.dataframe(df_struct_simplifie, hide_index=True)
