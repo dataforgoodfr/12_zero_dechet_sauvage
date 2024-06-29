0bb99067bbf2d1ceeb74ca2dfacdfbcb12d006e9
KyllianBeguin <kyllianbeguin@vivaldi.net>
2024-06-29 12:38:56 -0400

    [kb] 🚧 Lock files to csv format

--- dashboards/app/pages/upload_files.py
+++ dashboards/app/pages/upload_files.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
 from st_pages import Page, show_pages
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 # Configuration de la page
 st.set_page_config(
@@ -18,7 +18,11 @@ st.markdown(
 """
 )
 
-uploaded_file = st.file_uploader("Déposez votre fichier")
+uploaded_file = st.file_uploader("Déposez votre fichier", type="csv")
 
-if st.button("OK"):
-    st.success(f"Le fichier {uploaded_file.name} déposé !", icon="🚀")
+if (isinstance(uploaded_file, UploadedFile)) and ("csv" in uploaded_file.name):
+    if st.button("OK"):
+        st.success(f"Le fichier {uploaded_file.name} déposé !", icon="🚀")
+
+else:
+    pass
